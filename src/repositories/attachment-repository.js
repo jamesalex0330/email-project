5524d8654f67bc8e3221c4eff5186d613503efe6
harishmahajan <harishm@neuronsolutions.com>
2023-04-17 14:30:09 +0530

    Add mailer

--- src/repositories/attachment-repository.js
+++ src/repositories/attachment-repository.js
@@ -1,14 +1,13 @@
 import bcrypt from "bcryptjs";
 import models from "../models";
+import service from "../services/mailer";
 
 const { Sequelize } = models.sequelize;
 
 export default {
   async getAttachment(req) {
     try {
-      const salt = await bcrypt.genSalt();
-      const hashPassword = await bcrypt.hash(password, salt);
-      return hashPassword;
+     
     } catch (error) {
       throw Error(error);
     }

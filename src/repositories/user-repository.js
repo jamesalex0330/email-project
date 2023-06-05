8af9bc090cad875acfbaacd0e431c8e091470660
Praveen Joshi <praveenj@codiant.com>
2023-06-05 21:51:34 +0530

    Changes in dashboard api

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -11,26 +11,47 @@ export default {
   async dashboard(req, t) {
     try {
       let data = {};
+      const { query } = req;
+      const queryData = query;
       let userId = req.user.id;
       let investedData = {};
-      const leadData = await UserLead.findOne({
-        where : {userId:userId},
-        attributes: [Sequelize.fn("SUM", Sequelize.col("amount"))],
-        raw: true
-      });
-      const csdData = await CdsHold.findOne({
-        attributes: [Sequelize.fn("SUM", Sequelize.col("currentValue"))],
-        raw: true
-      });
-      investedData.invested = leadData.sum;
-      investedData.current = csdData.sum;
-      let fundData = await CdsHold.findAll({
-        attributes: {
-          exclude: ["id", "can", "canName", "fundCode", "schemeCode", "schemeName", "folioNumber", "folioCheckDigit", "navDate",'createdAt','updatedAt'],
+      const leadData = await UserLead.findOne(
+        {
+          where: { userId: userId },
+          attributes: {
+            include: [
+              [Sequelize.fn('ROUND', Sequelize.fn('SUM', Sequelize.col('amount')), 2), 'sum_amount']
+            ]
+          },
+          group: ["id"],
+          raw: true
+        }
+      );
+      const csdData = await CdsHold.findOne(
+        {
+          attributes: {
+            include: [
+              [Sequelize.fn('ROUND', Sequelize.fn('SUM', Sequelize.col('currentValue')), 2), 'currentValue']
+            ]
+          },
+          group: ["id"],
+          raw: true
+        }
+      );
+      investedData.invested = leadData.sum_amount;
+      investedData.current = csdData.currentValue;
+      let fundData = await CdsHold.findAndCountAll(
+        {
+          attributes: {
+            exclude: ["id", "canName", "fundCode", "schemeCode", "schemeName", "folioNumber", "folioCheckDigit", "navDate", 'createdAt', 'updatedAt'],
+          },
+          where: { 'can': leadData.canNumber },
+          limit: parseInt(queryData.limit || 10),
+          offset: parseInt(queryData.offset || 0)
         },
-      });
+      );
       data.userId = userId;
-      data.name = req.user.firstName+' '+req.user.lastName;
+      data.name = req.user.firstName + ' ' + req.user.lastName;
       data.investedData = investedData;
       data.investedData.fundData = fundData
       return data;
@@ -38,21 +59,21 @@ export default {
       throw Error(error);
     }
   },
-  
-    /**
-   * Find user detail
-   * @param {Object} whereObj
-   */
-     async findOne(whereObj) {
-      try {
-        return await User.findOne({
-          where: whereObj,
-          attributes: {
-            exclude: ["password", "verifyToken"],
-          },
-        });
-      } catch (error) {
-        throw Error(error);
-      }
+
+  /**
+ * Find user detail
+ * @param {Object} whereObj
+ */
+  async findOne(whereObj) {
+    try {
+      return await User.findOne({
+        where: whereObj,
+        attributes: {
+          exclude: ["password", "verifyToken"],
+        },
+      });
+    } catch (error) {
+      throw Error(error);
     }
+  }
 }

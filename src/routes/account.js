3ce3dac67eb06750dcbac57ac75917fed6dbd940
Praveen Joshi <praveenj@codiant.com>
2024-01-18 18:45:05 +0530

    Changes in routes

--- src/routes/account.js
+++ src/routes/account.js
@@ -10,26 +10,26 @@ const { validateMiddleware } = middlewares
 router.post("/signup",
     validateMiddleware({ schema: accountValidator.userAccountSignupSchema }),
     accountController.signup
-)
+);
 router.post("/login",
 validateMiddleware({ schema: accountValidator.userAccountLoginSchema }),
     accountController.userAccountLogin
-)
+);
 router.post("/forgot-password",
     validateMiddleware({ schema: accountValidator.sendOtpSchema }),
     accountController.sendOtp
-)
+);
 router.post("/resend-otp",
     validateMiddleware({ schema: accountValidator.sendOtpSchema }),
     accountController.sendOtp
-)
+);
 router.post("/verify-otp",
     validateMiddleware({ schema: accountValidator.verifyOtpSchema }),
     accountController.verifyOtp
-)
+);
 router.get("/logout",
     authMiddleware,
     accountController.logout
-)
+);
 
 export default router;
